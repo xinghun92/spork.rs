fa9bdb81024e8b5cee963188ba417291e11868b7
wangsijie <wangsijie@bytedance.com>
2018-08-05 14:39:41 +0800

    feat: update bench statistics
    Change-Id: I531d3d5d7ff32c6fb2dbedac068f3c9ace4b9234

--- benches/processes.rs
+++ benches/processes.rs
@@ -8,7 +8,7 @@ use spork::{Spork, StatType};
 #[bench]
 fn bench_get_stats(b: &mut ::test::Bencher) {
     // darwin: 2,801 ns/iter (+/- 379)
-    // linux: 434 ns/iter (+/- 3)
+    // linux: 241,176 ns/iter (+/- 2,009)
     // windows: 478 ns/iter (+/- 10)
     let spork = Spork::new().unwrap();
 
